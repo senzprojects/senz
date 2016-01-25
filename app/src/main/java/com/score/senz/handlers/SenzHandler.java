23b98b8ffd1a33b93e5d748c0627fcabb7145dec
eranga herath <erangaeb@gmail.com>
2016-01-25 16:52:48 +0530

    tweak

--- app/src/main/java/com/score/senz/handlers/SenzHandler.java
+++ app/src/main/java/com/score/senz/handlers/SenzHandler.java
@@ -7,12 +7,6 @@ import android.util.Log;
 import com.score.senz.utils.SenzParser;
 import com.score.senzc.pojos.Senz;
 
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SignatureException;
-
-//import com.score.senz.db.SenzorsDbSource;
-
 /**
  * Handle All senz messages from here
  */
@@ -31,84 +25,40 @@ public class SenzHandler {
             instance = new SenzHandler();
             SenzHandler.context = context;
         }
+
         return instance;
     }
 
     public void handleSenz(String senzMessage) {
-        try {
-            // parse and verify senz
-            Senz senz = SenzParser.parse(senzMessage);
-            verifySenz(senz);
-            switch (senz.getSenzType()) {
-                case PING:
-                    Log.d(TAG, "PING received");
-                    break;
-                case SHARE:
-                    Log.d(TAG, "SHARE received");
-                    handleShareSenz(senz);
-                    break;
-                case GET:
-                    Log.d(TAG, "GET received");
-                    handleGetSenz(senz);
-                    break;
-                case DATA:
-                    Log.d(TAG, "DATA received");
-                    handleDataSenz(senz);
-                    break;
-                case PUT:
-                    Log.d(TAG, "PUT received");
-                    handleDataSenz(senz);
-                    break;
-            }
-        } catch (NoSuchAlgorithmException | InvalidKeyException | SignatureException e) {
-            e.printStackTrace();
+        // parse and verify senz
+        Senz senz = SenzParser.parse(senzMessage);
+        switch (senz.getSenzType()) {
+            case PING:
+                Log.d(TAG, "PING received");
+                break;
+            case SHARE:
+                Log.d(TAG, "SHARE received");
+                broadcastSenz(senz);
+                break;
+            case GET:
+                Log.d(TAG, "GET received");
+                broadcastSenz(senz);
+                break;
+            case DATA:
+                Log.d(TAG, "DATA received");
+                broadcastSenz(senz);
+                break;
+            case PUT:
+                Log.d(TAG, "PUT received");
+                broadcastSenz(senz);
+                break;
         }
     }
 
-    private static void verifySenz(Senz senz) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
-        senz.getSender();
-
-        // TODO get public key of sender
-        // TODO verify signature of the senz
-        //RSAUtils.verifyDigitalSignature(senz.getPayload(), senz.getSignature(), null);
-    }
-
-    private void handleShareSenz(Senz senz) {
+    private void broadcastSenz(Senz senz) {
         Intent newSenzIntent = new Intent("com.score.senz.NEW_SENZ");
         newSenzIntent.putExtra("SENZ", senz);
         context.sendBroadcast(newSenzIntent);
     }
-
-    private void handleGetSenz(Senz senz) {
-
-        Intent newSenzIntent = new Intent("com.score.senz.NEW_SENZ");
-        newSenzIntent.putExtra("SENZ", senz);
-        context.sendBroadcast(newSenzIntent);
-    }
-
-    private void handlePutSenz(Senz senz) {
-
-        Intent newSenzIntent = new Intent("com.score.senz.NEW_SENZ");
-        newSenzIntent.putExtra("SENZ", senz);
-        context.sendBroadcast(newSenzIntent);
-    }
-
-    private void handleDataSenz(Senz senz) {
-        // sync data with db data
-        ///SenzorsDbSource dbSource = new SenzorsDbSource(context); ToDo db
-        ///User sender = dbSource.getOrCreateUser(senz.getSender().getUsername());
-        ///senz.setSender(sender);
-
-        // we broadcast data senz
-//        Intent intent = new Intent("DATA");
-//        intent.putExtra("SENZ", senz);
-//        context.sendBroadcast(intent);
-
-        // broadcast received senz
-        Intent newSenzIntent = new Intent("com.score.senz.DATA");
-        newSenzIntent.putExtra("SENZ", senz);
-        context.sendBroadcast(newSenzIntent);
-    }
-
 
 }
